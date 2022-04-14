7532e132d83cb6a581eee3706a1d906c474cb2ba
jonahsly <slyjonatan@gmail.com>
2022-04-14 19:48:07 -0300

    Updates

---
+++ precache-manifest.569ac6d27fa67809858c2ec4282926d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c31d7baf35b0a63a7a50",
+    "url": "/TDM/static/css/main.09d749a3.chunk.css"
+  },
+  {
+    "revision": "c31d7baf35b0a63a7a50",
+    "url": "/TDM/static/js/main.c31d7baf.chunk.js"
+  },
+  {
+    "revision": "9ee8710d55796a96507c",
+    "url": "/TDM/static/js/1.9ee8710d.chunk.js"
+  },
+  {
+    "revision": "7be22e76641eb493c8fa",
+    "url": "/TDM/static/js/runtime~main.7be22e76.js"
+  },
+  {
+    "revision": "009285a14d76c312f97bf7deaf434939",
+    "url": "/TDM/index.html"
+  }
+];
